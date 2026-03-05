fdbf725f89b80e7f5c3ac2b4c795fd1c2b161f26
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2026-03-05 15:13:50 +0100

    Enhance pdf text extraction pipeline

--- ml_classification/dataset_creation.py
+++ ml_classification/dataset_creation.py
@@ -3,26 +3,19 @@ import re
 from pathlib import Path
 
 import polars as pl
+import pymupdf.layout  # noqa: F401
+import pymupdf4llm
 from tqdm import tqdm
 
-from .utils import get_raw_text_pymupdf
 
-
-def clean_text(raw_test: str) -> str:
-    text = raw_test.replace("\xa0", " ")
-    text = text.replace("\x0c", " ")
-    text = text.replace("\n", " ")
-    ## Deletes duplicates whitesapces
-    text = re.sub(r" {2,}", " ", text)
-    return text
-
-
-def extract_text(pdf_path: Path) -> str | None:
-    if not pdf_path.exists():
+def clean_text(raw_text: str | None) -> str | None:
+    if raw_text is None:
         return None
-    raw_text = get_raw_text_pymupdf(pdf_path)
-
-    return clean_text(raw_text)
+    text = re.sub(
+        r"==> picture \[[0-9]{,4} x [0-9]{,4}\] intentionally omitted <==", "", raw_text
+    )  # Remove image placeholders
+    text = re.sub(r" {2,}", " ", text)  # Remove duplicate white spaces
+    return text
 
 
 def extract_context_index_positions(
@@ -51,14 +44,24 @@ def create_dataset(
         pdfs_to_select = pdfs_labels_df.get_column("pdf_name").unique().to_list()
 
     pdfs_dicts = []
-    for pdf_path in tqdm(pdf_dir.glob("*.pdf"), desc="Reading PDFs"):
+
+    for pdf_path in tqdm(pdf_dir.glob("*.pdf"), desc="Reading PDFs", unit=" PDFs read"):
         pdf_name = pdf_path.name
         if (pdfs_to_select is not None) and (pdf_name not in pdfs_to_select):
             continue
 
-        pdf_text = extract_text(pdf_path)
+        md_text: str = pymupdf4llm.to_markdown(
+            pdf_path,
+            header=False,
+            footer=False,
+            ignore_code=True,
+            ignore_graphics=True,
+            ignore_images=True,
+        )  # type: ignore
 
-        pdfs_dicts.append({"pdf_name": pdf_name, "pdf_text": pdf_text})
+        pdf_text_clean = clean_text(md_text)
+
+        pdfs_dicts.append({"pdf_name": pdf_name, "pdf_text": pdf_text_clean})
 
     preprocessed_pdfs_df = pl.DataFrame(pdfs_dicts)
 
@@ -105,7 +108,7 @@ if __name__ == "__main__":
     arg_parser.add_argument("output_path", help="Path where to output parquet file.")
 
     arg_parser.add_argument(
-        "--labels_dataset",
+        "--labels-dataset",
         help="CSV dataset containing labels for each PDF. Needed to create a preprocessed file with labels for model training or evaluation.",
     )
 
